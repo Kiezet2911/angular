3dd1c383ba7528c2ea0eb84fc4d754ec9a4c6c3f
Kiezet2911 <kiezetnguyen@gmail.com>
2022-03-24 15:30:33 +0700

    up

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/angular"));
+app.use(express.static("../dist/mbook"));
 
 app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "src/" })
+  res.sendFile("index.html", { root: "../dist/mbook" })
 );
 
 // Start the app by listening on the default Heroku port
