559f68094ec9553b9eaddf5b91adfb037841cf49
Kiezet2911 <kiezetnguyen@gmail.com>
2022-03-24 17:01:08 +0700

    up

--- src/app/pages/cartinfo/cartinfo.component.ts
+++ src/app/pages/cartinfo/cartinfo.component.ts
@@ -25,7 +25,7 @@ export class CartinfoComponent implements OnInit {
 
   actionIn_ngOnInit() {
     sessionStorage.removeItem("Mess")
-    this.Date = new Date().toLocaleDateString();
+    this.Date = new Date();
     this.getbook();
     this.Sum();
     this.checkUserLogin();
@@ -75,6 +75,7 @@ export class CartinfoComponent implements OnInit {
           }
           //
           let Body = new reqDatHangnodategiao(false, false, this.Date, this.Total, data.id, arrayidBook, arraycountBook);
+          console.log(Body)
           this.bookapi.DatHang(Body).subscribe(data => {
             //Nếu Có biến Mess Tức Có Sách Bị Vượt Quá Số Lượng Hiện Có Trong Kho Hàng
             if (data.Messager != null) {
@@ -88,7 +89,7 @@ export class CartinfoComponent implements OnInit {
                   } else {
                     this.mess = "Đặt Hàng Thành Công\nXin Vui Lòng Kiểm Tra Lịch Sử Mua Hàng"
                     if (this.UserLogin[0].Email != null || typeof this.UserLogin[0].Email != "undefined" || this.UserLogin[0].Email != "") {
-                      this.bookapi.SendMail(this.UserLogin[0].Email).subscribe(res => {                     
+                      this.bookapi.SendMail(this.UserLogin[0].Email).subscribe(res => {
                       })
                     }
                     sessionStorage.removeItem("listCart")
